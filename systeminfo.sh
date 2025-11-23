0e8bfa1d7a24eaa17117c80ccf7ff1f4c18b4ee5
MichaelDuelos <e3017837@student.dcccd.edu>
2025-11-23 11:58:00 -0600

    Updated systeminfo.sh script to remove unusable commands

--- systeminfo.sh
+++ systeminfo.sh
@@ -6,8 +6,6 @@ echo "------------------"
 echo "Hostname: $(hostname)"
 echo "Kernel Version: $(uname -r)"
 echo "Operating System: $(uname -o)"
-echo "Uptime:"
-uptime
 
 echo ""
 echo "Disk Usage:"
